f4cfae1d4ba26f2cfc6e737bdeaa5578c97f6e05
memokey <skytopground@gmail.com>
2022-08-08 15:00:52 +0800

    complete login and registration

--- src/popup/Popup.jsx
+++ src/popup/Popup.jsx
@@ -1,47 +1,50 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from "react-redux";
 
-import LandingPage from './pages/LandingPage';
-import UserInfoPage from './pages/auth/UserInfoPage';
-import UserPic from './pages/auth/UserPic';
+// import LandingPage from './pages/LandingPage';
+// import UserInfoPage from './pages/auth/UserInfoPage';
+// import UserPic from './pages/auth/UserPic';
+import ProfilePage from './pages/ProfilePage';
 
-import { setPageStages, login, userExist } from './redux/slices/authSlice';
+// import { setPageStages, login, userExist } from './redux/slices/authSlice';
 
 import './Popup.css';
-import { setValue } from './utils';
+// import { setValue } from './utils';
 
 const Popup = () => {
   const [loginStages, setLoginStages] = useState(0);
   const [solanaAddress, setSolanaAddress] = useState("");
+  const [pageStages, setPageStages] = useState(3);
 
-  const { pageStages, authFlag, profileData } = useSelector((state) => ({
-    profileData: state.auth.profile,
-    pageStages: state.auth.pageStages,
-    authFlag: state.auth.authFlag,
-  }));
-  const dispatch = useDispatch();
+  // const { pageStages, authFlag, profileData } = useSelector((state) => ({
+  //   profileData: state.auth.profile,
+  //   pageStages: state.auth.pageStages,
+  //   authFlag: state.auth.authFlag,
+  // }));
+  // const dispatch = useDispatch();
 
-  useEffect(() => {
-    chrome.runtime.onMessage.addListener(onExtMessage);
-  }, [])
+  // useEffect(() => {
+  //   chrome.runtime.onMessage.addListener(onExtMessage);
+  // }, [])
 
-  const onExtMessage = (message, sender, sendResponse) => {
-    switch (message.command) {
-      case 'wallet-connected':
-        dispatch(userExist({ publicKey: message.publicKey, walletType: setValue(localStorage.getItem('walletType')) }));
-        break;
-      case 'receive-signature':
-        dispatch(login({ publicKey: message.publicKey, walletType: message.walletType, signature: message.signature }))
-        break;
-    }
-  }
+  // const onExtMessage = (message, sender, sendResponse) => {
+  //   switch (message.command) {
+  //     case 'wallet-connected':
+  //       dispatch(userExist({ publicKey: message.publicKey, walletType: setValue(localStorage.getItem('walletType')) }));
+  //       break;
+  //     case 'receive-signature':
+  //       dispatch(login({ publicKey: message.publicKey, walletType: message.walletType, signature: message.signature }))
+  //       break;
+  //   }
+  // }
 
   return (
     <div className='w-[400px] h-[650px] rounded-lg bg-[#141414]'>
       {/* login and register */}
-      {pageStages == 0 && <LandingPage />}
+      {/* {pageStages == 0 && <LandingPage />}
       {pageStages == 1 && <UserInfoPage />}
-      {pageStages == 2 && <UserPic />}
+      {pageStages == 2 && <UserPic />} */}
+      {pageStages == 3 && <ProfilePage />}
       {/*  */}
     </div>
   );
